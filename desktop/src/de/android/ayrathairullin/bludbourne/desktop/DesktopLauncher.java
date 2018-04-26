222cd469dd3ee01c8dd676f7130003544caca25e
Ayrat <ayrat1@mail.ru>
2018-04-26 23:03:49 +0300

    Initial commit

---
+++ desktop/src/de/android/ayrathairullin/bludbourne/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.android.ayrathairullin.bludbourne.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.android.ayrathairullin.bludbourne.BludBourne;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BludBourne(), config);
+	}
+}
